bad663ec476c189612965c88218a66ee2ae57d12
Diego Tavares da Silva <diego.silva_19@yahoo.com.br>
2025-04-04 01:24:34 -0300

    adjust path and changes context - should be use one for each time

--- project/services/kube_service.go
+++ project/services/kube_service.go
@@ -40,9 +40,16 @@ func CreateList(w http.ResponseWriter, r *http.Request) {
 
 	for _, itemList := range payload {
 		clusterName := itemList.ClusterName
+		clusterCommand := "kubectx " + itemList.ClusterName
 		namespace := itemList.NameSpace
+		logsDir := itemList.LogDir
 
-		cmdStr := "kubectx " + itemList.ClusterName + " && kubectl get pods -n " + namespace + " -o json | jq -r '.items[] | select(.metadata.name | contains(\"" + namespace + "\")) | .metadata.name'"
+		if logsDir == "" {
+			logsDir = "logs/"
+		}
+
+		exec.Command("bash", "-c", "kubectx "+clusterCommand).Run()
+		cmdStr := "kubectl get pods -n " + namespace + " -o json | jq -r '.items[] | select(.metadata.name | contains(\"" + namespace + "\")) | .metadata.name'"
 		cmd := exec.Command("bash", "-c", cmdStr)
 		stdout, err := cmd.StdoutPipe()
 
@@ -62,24 +69,26 @@ func CreateList(w http.ResponseWriter, r *http.Request) {
 
 			partsPodName := strings.Split(line, "-")
 
-			if len(partsPodName) == 3 {
+			if len(partsPodName) >= 3 {
 
-				appName := partsPodName[0]
-				deployVersion := partsPodName[1]
-				podNameHash := partsPodName[2]
+				deployVersion := partsPodName[len(partsPodName)-2]
 
+				podNameHash := partsPodName[len(partsPodName)-1]
+
+				appName := strings.Join(partsPodName[0:len(partsPodName)-2], "-")
 				podInfo := KubePod{
+					Name:          line,
 					Namespace:     namespace,
 					AppName:       appName,
 					ClusterName:   clusterName,
 					DeployVersion: deployVersion,
 					PodNameHash:   podNameHash,
-					LogCommandStr: "kubectl logs -f " + line,
-					WriteFile:     itemList.LogDir + "/" + appName + "-" + deployVersion + "-" + podNameHash + ".log",
+					LogCommandStr: "kubectl logs -n " + namespace + " -f " + line,
+					WriteFile:     logsDir + "/" + appName + "-" + deployVersion + "-" + podNameHash + ".log",
 				}
 
 				if !itemList.SeparatedFile {
-					podInfo.WriteFile = "logs/" + appName + ".log"
+					podInfo.WriteFile = logsDir + "/" + appName + ".log"
 				}
 				responses = append(responses, podInfo)
 			}
